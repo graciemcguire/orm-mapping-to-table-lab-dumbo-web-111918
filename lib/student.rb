fd73f69ec3dd4d793f8b343a9dcee59676c8d227
Gracie McGuire <m.graciemcguire@gmail.com>
2018-12-02 18:19:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id 
+  end
+  
+  def.create_table 
+    
   
 end
